9941230bc4b15019cec3fb1862066c4ec97d36cf
John Garbutt <john.garbutt@stackhpc.com>
2023-01-06 15:07:48 +0000

    Split out register crds

--- azimuth_caas_operator/operator.py
+++ azimuth_caas_operator/operator.py
@@ -17,15 +17,19 @@ def get_k8s_client():
     ).async_client(default_field_manager=easykube_field_manager)
 
 
-@kopf.on.startup()
-async def startup(**kwargs):
-    LOG.info("Starting")
+async def register_crds(client):
     reg = registry.get_registry()
-    client = get_k8s_client()
     for crd in reg:
         resource = crd.kubernetes_resource()
         await client.apply_object(resource, force=True)
     LOG.info("CRDs imported")
+
+
+@kopf.on.startup()
+async def startup(**kwargs):
+    LOG.info("Starting")
+    client = get_k8s_client()
+    await register_crds(client)
 
 
 @kopf.on.cleanup()

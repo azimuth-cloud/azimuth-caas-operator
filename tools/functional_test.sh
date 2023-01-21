9872b1992299f84dc22b73ffa10b9bdd95f102e5
John Garbutt <john.garbutt@stackhpc.com>
2023-01-21 07:30:39 +0000

    Fix ups to functional testing

--- tools/functional_test.sh
+++ tools/functional_test.sh
@@ -5,16 +5,25 @@ set -eux
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 tox -e kopf &
-until [ `kubectl get crds | grep cluster | wc -l` -gt 2 ]; do echo "wait for crds"; done
+until [ `kubectl get crds | grep cluster | wc -l` -gt 1 ]; do echo "wait for crds"; done
 
-kubectl create -f $SCRIPT_DIR/test_cluster_type.yaml
+kubectl apply -f $SCRIPT_DIR/test_cluster_type.yaml
 kubectl create -f $SCRIPT_DIR/test_quick.yaml
 
 kubectl wait --for=jsonpath='{.status.phase}'=Creating cluster quick-test
 kubectl wait --for=jsonpath='{.status.phase}'=Ready cluster quick-test
 
-kubectl delete -f $SCRIPT_DIR/test_quick.yam
+kubectl get cluster
+kubectl get clustertype
+kubectl get jobs
+kubectl get pods
+
+kubectl get jobs -o yaml
+kubectl get pods -o yaml
+
+kubectl delete -f $SCRIPT_DIR/test_quick.yaml --wait=false
 kubectl wait --for=jsonpath='{.status.phase}'=Deleting cluster quick-test
 kubectl wait --for=delete cluster quick-test
 
+kubectl get cluster
 kill %1

8d9e0260c3232031aaf3b6ffa44c1acae6c1c13b
John Garbutt <john.garbutt@stackhpc.com>
2023-01-21 08:11:13 +0000

    Wait longer for create

--- tools/functional_test.sh
+++ tools/functional_test.sh
@@ -14,17 +14,23 @@ kubectl apply -f $SCRIPT_DIR/test_cluster_type.yaml
 kubectl create -f $SCRIPT_DIR/test_quick.yaml
 
 until kubectl wait --for=jsonpath='{.status.phase}'=Creating cluster quick-test; do echo "wait for status to appear"; sleep 5; done
-kubectl wait --for=jsonpath='{.status.phase}'=Ready cluster quick-test
 
 kubectl get cluster
 kubectl get clustertype
 kubectl get jobs
 kubectl get pods
 
+until kubectl wait --for=jsonpath='{.status.phase}'=Ready cluster quick-test; do echo "wait for status to appear"; sleep 5; done
+
+kubectl get cluster
+kubectl get clustertype
+kubectl get jobs
+kubectl get pods
 kubectl get jobs -o yaml
 kubectl get pods -o yaml
 
-sleep 10
 kubectl delete -f $SCRIPT_DIR/test_quick.yaml
+sleep 10
+
 kubectl get cluster
 kill %1

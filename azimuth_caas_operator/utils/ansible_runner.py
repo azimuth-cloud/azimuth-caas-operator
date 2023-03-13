2ac6b92b8a2039cb304892a9ded3843337ed2e9b
John Garbutt <john.garbutt@stackhpc.com>
2023-03-13 17:38:30 +0000

    Remove empty space from public key

--- azimuth_caas_operator/utils/ansible_runner.py
+++ azimuth_caas_operator/utils/ansible_runner.py
@@ -259,7 +259,7 @@ async def start_job(client, cluster, namespace, remove=False):
 
     cluster_deploy_ssh_public_key = base64.b64decode(
         ssh_secret.data.get("id_rsa.pub")
-    ).decode("utf-8")
+    ).decode("utf-8").strip()
     # generate config
     configmap_data = get_env_configmap(
         cluster,

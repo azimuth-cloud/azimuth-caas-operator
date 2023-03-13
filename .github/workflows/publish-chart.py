4ac3d352fdef498c91f90d5276d7dc46193e2c63
John Garbutt <john.garbutt@stackhpc.com>
2023-03-13 10:02:24 +0000

    Fix tox tests with dodgy publish-chart script

--- .github/workflows/publish-chart.py
+++ .github/workflows/publish-chart.py
@@ -28,7 +28,6 @@ import subprocess
 import tempfile
 
 
-
 @contextlib.contextmanager
 def working_directory(directory):
     """
@@ -49,7 +48,7 @@ def cmd(command):
     """
     Execute the given command and return the output.
     """
-    output = subprocess.check_output(command, text = True, stderr = subprocess.DEVNULL)
+    output = subprocess.check_output(command, text=True, stderr=subprocess.DEVNULL)
     return output.strip()
 
 
@@ -63,7 +62,7 @@ def get_version():
     Returns a (version, app_version) tuple where version is a SemVer-compliant version based on
     Git information for the current working directory and app_version is the short-sha as used
     to tag the operator image.
-    
+
     The version is based on the distance from the last tag and includes the name of the branch that the
     commit is on. It is is constructed such that the versions for a particular branch will order correctly.
     """
@@ -85,11 +84,11 @@ def get_version():
         # If we found a tag, split into major/minor/patch/prerelease
         tag_bits = re.search(SEMVER_REGEX, last_tag)
         if tag_bits is None:
-            raise RuntimeError(f'Tag is not a valid SemVer version - {last_tag}')
-        major_vn = int(tag_bits.group('major'))
-        minor_vn = int(tag_bits.group('minor'))
-        patch_vn = int(tag_bits.group('patch'))
-        prerelease_vn = tag_bits.group('prerelease')
+            raise RuntimeError(f"Tag is not a valid SemVer version - {last_tag}")
+        major_vn = int(tag_bits.group("major"))
+        minor_vn = int(tag_bits.group("minor"))
+        patch_vn = int(tag_bits.group("patch"))
+        prerelease_vn = tag_bits.group("prerelease")
         # Get the number of commits since the last tag
         commits = int(cmd(["git", "rev-list", "--count", f"{last_tag}..HEAD"]))
 
@@ -102,7 +101,7 @@ def get_version():
         branch_name = cmd(["git", "rev-parse", "--abbrev-ref", "HEAD"]).lower()
         # Sanitise the branch name so it only has characters valid for a prerelease version
         branch_name = re.sub("[^a-zA-Z0-9-]+", "", branch_name).lower()
-        prerelease_vn = '.'.join([prerelease_vn or "dev.0", branch_name, str(commits)])
+        prerelease_vn = ".".join([prerelease_vn or "dev.0", branch_name, str(commits)])
 
     # Build the SemVer version from the parts
     version = f"{major_vn}.{minor_vn}.{patch_vn}"
@@ -116,29 +115,31 @@ def setup_publish_branch(branch, publish_directory):
     """
     Clones the specified branch into the specified directory.
     """
-    server_url = os.environ['GITHUB_SERVER_URL']
-    repository = os.environ['GITHUB_REPOSITORY']
+    server_url = os.environ["GITHUB_SERVER_URL"]
+    repository = os.environ["GITHUB_REPOSITORY"]
     remote = f"{server_url}/{repository}.git"
     print(f"[INFO] Cloning {remote}@{branch} into {publish_directory}")
     # Try to clone the branch
     # If it fails, create a new empty git repo with the same remote
     try:
-        cmd([
-            'git',
-            'clone',
-            '--depth=1',
-            '--single-branch',
-            '--branch',
-            branch,
-            remote,
-            publish_directory
-        ])
+        cmd(
+            [
+                "git",
+                "clone",
+                "--depth=1",
+                "--single-branch",
+                "--branch",
+                branch,
+                remote,
+                publish_directory,
+            ]
+        )
     except subprocess.CalledProcessError:
         with working_directory(publish_directory):
-            cmd(['git', 'init'])
-            cmd(['git', 'remote', 'add', 'origin', remote])
-            cmd(['git', 'checkout', '--orphan', branch])
-    username = os.environ['GITHUB_ACTOR']
+            cmd(["git", "init"])
+            cmd(["git", "remote", "add", "origin", remote])
+            cmd(["git", "checkout", "--orphan", branch])
+    username = os.environ["GITHUB_ACTOR"]
     email = f"{username}@users.noreply.github.com"
     with working_directory(publish_directory):
         print(f"[INFO] Configuring git to use username '{username}'")
@@ -147,12 +148,14 @@ def setup_publish_branch(branch, publish_directory):
         print("[INFO] Configuring git to use authentication token")
         # Basic auth credentials should be base64-encoded
         basic_auth = f"x-access-token:{os.environ['GITHUB_TOKEN']}"
-        cmd([
-            "git",
-            "config",
-            "http.extraheader",
-            f"Authorization: Basic {base64.b64encode(basic_auth.encode()).decode()}"
-        ])
+        cmd(
+            [
+                "git",
+                "config",
+                "http.extraheader",
+                f"Authorization: Basic {base64.b64encode(basic_auth.encode()).decode()}",
+            ]
+        )
 
 
 def main():
@@ -166,24 +169,26 @@ def main():
     version, app_version = get_version()
 
     # Publish the charts and re-generate the repository index
-    publish_branch = os.environ.get('PUBLISH_BRANCH', 'gh-pages')
+    publish_branch = os.environ.get("PUBLISH_BRANCH", "gh-pages")
     print(f"[INFO] Chart will be published to branch '{publish_branch}'")
     print(f"[INFO] Chart will be published with version '{version}'")
     with tempfile.TemporaryDirectory() as publish_directory:
         setup_publish_branch(publish_branch, publish_directory)
         print(f"[INFO] Packaging chart in {chart_directory}")
-        cmd([
-            "helm",
-            "package",
-            "--dependency-update",
-            "--version",
-            version,
-            "--app-version",
-            app_version,
-            "--destination",
-            publish_directory,
-            chart_directory
-        ])
+        cmd(
+            [
+                "helm",
+                "package",
+                "--dependency-update",
+                "--version",
+                version,
+                "--app-version",
+                app_version,
+                "--destination",
+                publish_directory,
+                chart_directory,
+            ]
+        )
         # Re-index the publish directory
         print("[INFO] Generating Helm repository index file")
         cmd(["helm", "repo", "index", publish_directory])

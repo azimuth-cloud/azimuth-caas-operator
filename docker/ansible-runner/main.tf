69f2b0820d0a032ea09189f6583e84294b842d86
John Garbutt <john.garbutt@stackhpc.com>
2023-04-27 09:24:34 +0100

    Fix the version pinning on terraform

--- docker/ansible-runner/main.tf
+++ docker/ansible-runner/main.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     openstack = {
       source = "terraform-provider-openstack/openstack"
-      version = "~> 1.50.2"
+      version = "~> 1.50.0"
     }
   }
 }

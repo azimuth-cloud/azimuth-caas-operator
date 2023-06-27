677f468a1cd9f57816f765d11930199818d4e5f0
John Garbutt <john.garbutt@stackhpc.com>
2023-06-27 18:40:08 +0100

    Update functional_test.sh with CONSUL_HTTP_ADDR
    try to fix the functional test

--- tools/functional_test.sh
+++ tools/functional_test.sh
@@ -3,7 +3,8 @@
 set -ex
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
+# set the required environment variable, before starting the operator
+export CONSUL_HTTP_ADDR="zenith-consul-server.zenith:8500"
 tox -e kopf &
 sleep 10
 

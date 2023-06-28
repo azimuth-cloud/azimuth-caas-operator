18b3fa94f2dcafac11d70289666950c840acaab4
Matt Pryor <matt@stackhpc.com>
2023-06-28 10:12:09 +0100

    Use github ref name as tag in functional test

--- azimuth_caas_operator/utils/image.py
+++ azimuth_caas_operator/utils/image.py
@@ -1,7 +1,20 @@
 import os
 
-DEFAULT_ANSIBLE_RUNNER_IMAGE = "ghcr.io/stackhpc/azimuth-caas-operator-ee:v0.1.0"
+
+DEFAULT_ANSIBLE_RUNNER_IMAGE_REPO = "ghcr.io/stackhpc/azimuth-caas-operator-ee"
+DEFAULT_ANSIBLE_RUNNER_IMAGE_TAG = "latest"
 
 
 def get_ansible_runner_image():
-    return os.environ.get("ANSIBLE_RUNNER_IMAGE", DEFAULT_ANSIBLE_RUNNER_IMAGE)
+    if "ANSIBLE_RUNNER_IMAGE" in os.environ:
+        return os.environ["ANSIBLE_RUNNER_IMAGE"]
+    else:
+        repo = os.environ.get(
+            "ANSIBLE_RUNNER_IMAGE_REPO",
+            DEFAULT_ANSIBLE_RUNNER_IMAGE_REPO
+        )
+        tag = os.environ.get(
+            "ANSIBLE_RUNNER_IMAGE_TAG",
+            DEFAULT_ANSIBLE_RUNNER_IMAGE_TAG
+        )
+        return f"{repo}:{tag}"

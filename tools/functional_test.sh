9ca738f559275b03c1e6c542b15267894df8dd75
John Garbutt <john.garbutt@stackhpc.com>
2023-11-10 15:39:58 +0000

    Remove debug stuff

--- tools/functional_test.sh
+++ tools/functional_test.sh
@@ -29,10 +29,6 @@ until [ `kubectl get crds | grep cluster | wc -l` -gt 1 ]; do echo "wait for crd
 kubectl get crds
 
 kubectl apply -f $SCRIPT_DIR/test_cluster_type.yaml
-sleep 10
-kubectl get pods -n azimuth-caas-operator
-kubectl wait --for=jsonpath='{.status.phase}'=Available clustertype quick-test || true
-kubectl logs -n azimuth-caas-operator deployment/azimuth-caas-operator
 until kubectl wait --for=jsonpath='{.status.phase}'=Available clustertype quick-test; do echo "wait for status to appear"; sleep 5; done
 kubectl get clustertype quick-test -o yaml
 

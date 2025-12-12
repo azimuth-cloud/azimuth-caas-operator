047f34e96eda8e4dd58632a1908ec88073361846
John Garbutt <john.garbutt@stackhpc.com>
2025-12-12 11:13:05 +0000

    Remove test tweaks we don't need any more

--- azimuth_caas_operator/tests/utils/test_lease.py
+++ azimuth_caas_operator/tests/utils/test_lease.py
@@ -142,7 +142,6 @@ class TestLease(unittest.IsolatedAsyncioTestCase):
                     "name": "test1",
                     "namespace": "ns1",
                     "finalizers": ["caas.azimuth.stackhpc.com"],
-                    "resourceVersion": "12345",
                 },
             }
         )
@@ -166,7 +165,6 @@ class TestLease(unittest.IsolatedAsyncioTestCase):
                     "name": "test1",
                     "namespace": "ns1",
                     "finalizers": ["another-finalizer", "caas.azimuth.stackhpc.com"],
-                    "resourceVersion": "12345",
                 },
             }
         )
